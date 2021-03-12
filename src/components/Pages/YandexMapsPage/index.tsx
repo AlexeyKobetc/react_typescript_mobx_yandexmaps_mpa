ade420027bb4de4d98bb4243451dafdf906b8101
AlexeyKobetc <alexeyk@rambler.ru>
2021-03-12 14:03:14 +0300

    same

--- src/components/Pages/YandexMapsPage/index.tsx
+++ src/components/Pages/YandexMapsPage/index.tsx
@@ -19,6 +19,33 @@ const fadeOut = (root: HTMLDivElement) => {
   }
 };
 
+const changeButtonLabel = (event: any) => {
+  const { target, type } = event;
+
+  const { id } = target;
+
+  if (type === "shown.bs.collapse") {
+    if (id === "orderDiv") {
+      const orderButton = document.querySelector("#orderButton");
+      if (orderButton) orderButton.textContent = `unfold_less`;
+    }
+    if (id === "mapDiv") {
+      const mapButton = document.querySelector("#mapButton");
+      if (mapButton) mapButton.textContent = `unfold_less`;
+    }
+  }
+  if (type === "hidden.bs.collapse") {
+    if (id === "orderDiv") {
+      const orderButton = document.querySelector("#orderButton");
+      if (orderButton) orderButton.textContent = `unfold_more`;
+    }
+    if (id === "mapDiv") {
+      const mapButton = document.querySelector("#mapButton");
+      if (mapButton) mapButton.textContent = `unfold_more`;
+    }
+  }
+};
+
 const YandexMapsPage = observer(() => {
   const { setYmDiv, getIsYmReady } = useContextYandexMapsStore();
 
@@ -29,23 +56,38 @@ const YandexMapsPage = observer(() => {
       setYmDiv(yandexMapsDiv.current);
       getIsYmReady && fadeOut(yandexMapsDiv.current);
     }
+    const orderDiv = document.querySelector("#orderDiv");
+    const mapDiv = document.querySelector("#mapDiv");
+
+    orderDiv?.addEventListener("hidden.bs.collapse", changeButtonLabel);
+    orderDiv?.addEventListener("shown.bs.collapse", changeButtonLabel);
+    mapDiv?.addEventListener("hidden.bs.collapse", changeButtonLabel);
+    mapDiv?.addEventListener("shown.bs.collapse", changeButtonLabel);
+    return () => {
+      orderDiv?.removeEventListener("hidden.bs.collapse", changeButtonLabel);
+      orderDiv?.removeEventListener("shown.bs.collapse", changeButtonLabel);
+      mapDiv?.removeEventListener("hidden.bs.collapse", changeButtonLabel);
+      mapDiv?.removeEventListener("shown.bs.collapse", changeButtonLabel);
+    };
   });
 
   return (
     <React.Fragment>
       <div className="row d-flex justify-content-center align-items-center">
-        <div className="col-12 col-sm-11 mt-3 mb-3 d-grid">
-          <button
-            className="btn btn-outline-secondary"
-            type="button"
+        <div className="col-12 col-sm-11 mt-3 mb-3">
+          <span
+            className="btn btn-outline-secondary btn-sm material-icons"
+            style={{ width: "2rem" }}
+            id="orderButton"
             data-bs-toggle="collapse"
-            data-bs-target="#order"
+            data-bs-target="#orderDiv"
             aria-expanded="false"
             aria-controls="collapseExample"
           >
-            Раскрыть форму заказа
-          </button>
-          <div className="collapse" id="order">
+            unfold_less
+          </span>
+
+          <div className="collapse show" id="orderDiv">
             <OrderForm />
           </div>
         </div>
@@ -62,20 +104,21 @@ const YandexMapsPage = observer(() => {
       </div>
       <div className="row d-flex justify-content-center align-items-center">
         <div className="col-12 col-sm-11 mt-3 mb-3 d-grid">
-          <button
-            className="btn btn-outline-secondary"
-            type="button"
+          <span
+            className="btn btn-outline-secondary btn-sm material-icons"
+            style={{ width: "2rem" }}
+            id="mapButton"
             data-bs-toggle="collapse"
-            data-bs-target="#map"
+            data-bs-target="#mapDiv"
             aria-expanded="false"
             aria-controls="collapseExample"
           >
-            Раскрыть карту
-          </button>
+            unfold_less
+          </span>
         </div>
       </div>
       <div className="row d-flex justify-content-center align-items-center">
-        <div className="collapse" id="map">
+        <div className="collapse show" id="mapDiv">
           <div
             className="col-12 col-sm-11"
             style={{ position: "relative", minHeight: "80vh" }}
